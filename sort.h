ff978239dbd94ddeea8209f0ab37c2501a44ffa5
Oduse David <89360467+KingDavidJnr@users.noreply.github.com>
2023-04-21 10:12:25 +0000

    updated sort header file

--- sort.h
+++ sort.h
@@ -4,11 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Comparison direction macros for bitonic sort*/
+/*Initilalizing the Comparing direction macros for the bitonic sort*/
 #define UP 0
 #define DOWN 1
 
-/*enum bool - Enumeration of boolean values*/
+/*enum bool - Enumeration of the boolean values*/
 typedef enum bool
 {
     false = 0,
@@ -17,11 +17,11 @@ typedef enum bool
 
 
 /**
- * struct listint_s - Doubly linked list node
+ * struct listint_s - Initialize Doubly linked list node
  *
- * @n: Integer stored in the node
- * @prev: Pointer to the previous element of the list
- * @next: Pointer to the next element of the list
+ * @n: The Integer stored inside the node
+ * @prev: The Pointer to the previous element in the list
+ * @next: The Pointer to the next element in the list
  */
 typedef struct listint_s
 {
@@ -30,19 +30,17 @@ typedef struct listint_s
     struct listint_s *next;
 } listint_t;
 
-/*Helper for swapping*/
+/*Helper func for the swapping*/
 void swap_ints(int *a, int *b);
 
-/*Printing helper functions */
+/*Printing the helper functions */
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/*Regular functions prototypes*/
+/*Initialize the Regular functions prototypes*/
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-
-
 
 #endif
