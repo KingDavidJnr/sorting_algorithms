58f61111c1bbfe8704aeb9ac8fdb5d746150b674
Oduse David <89360467+KingDavidJnr@users.noreply.github.com>
2023-04-23 14:36:45 +0000

    correct shell sort

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,37 +1,26 @@
 #include "sort.h"
 
 /**
-  * shell_sort - The function that sorts an array using the shell algorithm.
-  * @array: The array to be sorted.
-  * @size: The size of the array.
-  * Return: Null.
-  */
-
+ * shell_sort - Shell sort - Knuth Sequence
+ * @array: The array to sort
+ * @size: The size of the array
+ */
 void shell_sort(int *array, size_t size)
 {
-	unsigned int i = 0, j = 0, gap = 0;
-	int aux = 0;
+	int gap = 1, i, j, tmp;
 
-	if (array == NULL || size < 2)
-		return;
-
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
-
-	for (; gap > 0; gap = (gap - 1) / 3)
+	while (gap < ((int)(size)))
+		gap = (3 * gap) + 1;
+	for (gap = (gap - 1) / 3; gap > 0; gap = (gap - 1) / 3)
 	{
-		for (i = gap; i < size; i++)
+		for (i = gap; i < (int)size; i += 1)
 		{
-			aux = array[i];
-			for (j = i; j >= gap && array[j - gap] > aux;
-					j -= gap)
-			{
-				if (array[j] != array[j - gap])
-					array[j] = array[j - gap];
-			}
-			if (array[j] != aux)
-				array[j] = aux;
+			tmp = array[i];
 
+			for (j = i; j >= gap && array[j - gap] > tmp; j -= gap)
+				array[j] = array[j - gap];
+
+			array[j] = tmp;
 		}
 		print_array(array, size);
 	}
